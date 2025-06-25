bced16adff2f4ef18628eb683617d720388d45c9
Samuel Newman <mozzius@protonmail.com>
2025-06-25 12:26:45 +0300

    add repro

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import {
+  createStaticNavigation,
+  Link,
+  useTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PagerView from "react-native-pager-view";
+
+function HomeScreen() {
+  const theme = useTheme();
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Link
+        screen="Pager"
+        style={{ color: theme.colors.primary, fontSize: 20 }}
+      >
+        Go to Pager
+      </Link>
+    </View>
+  );
+}
+function PagerScreen() {
+  return (
+    <PagerView style={styles.pv}>
+      <ScrollView
+        contentContainerStyle={styles.ccs}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <PlaceholderViews color="red" />
+      </ScrollView>
+      <ScrollView
+        contentContainerStyle={styles.ccs}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <PlaceholderViews color="green" />
+      </ScrollView>
+      <ScrollView
+        contentContainerStyle={styles.ccs}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <PlaceholderViews color="blue" />
+      </ScrollView>
+    </PagerView>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screenOptions: {
+    headerLargeTitle: true,
+    headerLargeStyle: {
+      backgroundColor: "transparent",
+    },
+    fullScreenGestureEnabled: true,
+  },
+  screens: {
+    Home: {
+      options: {
+        title: "Back swipe repro",
+      },
+      screen: HomeScreen,
+    },
+    Pager: {
+      screen: PagerScreen,
+    },
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
+  return <Navigation />;
+}
+
+function PlaceholderViews({ color }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {Array.from({ length: 50 }, (_, i) => (
+        <View
+          key={i}
+          style={{ width: "100%", height: 100, backgroundColor: color }}
+        />
+      ))}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pv: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  ccs: {
+    alignItems: "center",
+    gap: 20,
+    paddingHorizontal: 20,
   },
 });
